a81360d5b236d215ccc6f50013fee426cdb12368
Volodymyr Flonts <flyonts@gmail.com>
2015-09-19 23:30:04 +0300

    inital #2

---
+++ views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+from flask import *
+from flask_peewee.utils import get_object_or_404
+
+from app import app
+from models import *
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return ""
+
+
+@app.route('/')
+def main(template_name="main.html"):
+    return render_template(template_name)
+
+
+
