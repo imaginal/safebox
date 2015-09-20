8f88684082109e0f9c8bbcab903c2f326a3960b8
Volodymyr Flonts <flyonts@gmail.com>
2015-09-20 16:30:08 +0300

    login form

--- views.py
+++ views.py
@@ -26,20 +26,38 @@ def frame(template_name="frame.html"):
 
 @app.route('/signup', methods=['POST'])
 def signup():
+    full_name = request.form['FullName1']
     mobile = request.form['MobileNumber1']
     birth_date = request.form['BirthDate1']
-    birth_date = datetime.strptime(birth_date)
+    birth_date = datetime.strptime(birth_date, "%Y-%m-%d").date()
     pin_code = request.form['PinCode1']
     try:
         user = User.get(User.mobile==mobile)
     except:
-        user = User(mobile=mobile,
+        user = User(
+            full_name=full_name,
+            mobile=mobile,
             birth_date=birth_date,
             pin_code=pin_code)
         user.save()
     if user.pin_code == pin_code:
         session['uid'] = user.id
         return redirect('/docs')
+    return redirect('/')
+
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    mobile = request.form['MobileNumber1']
+    pin_code = request.form['PinCode1']
+    try:
+        user = User.get(User.mobile==mobile)
+        if user.pin_code == pin_code:
+            session['uid'] = user.id
+            return redirect('/docs')
+    except:
+        pass
     return redirect('/')
 
 
